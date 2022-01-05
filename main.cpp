c767f03ff2d2ae20e4f9d20166f4dcaf431e7a4e
Dmitriy Pavlov <ooo_tek@mail.ru>
2022-01-05 16:08:57 +0500

    Добавление операции сложения чисел

--- main.cpp
+++ main.cpp
@@ -7,4 +7,7 @@ int main()
 int a, b;
 cout << "Enter two integers, confirm the input with Enter" << endl;
 cin >> a >> b;
+int c;
+c = a + b;
+cout << "The sum of the entered numbers = " << c << endl;
 }
