f3f0f736778a915531038979e51f9189f64c2fbf
Dmitriy Pavlov <ooo_tek@mail.ru>
2022-01-05 21:59:42 +0500

    Добавлено деление чисел

--- main.cpp
+++ main.cpp
@@ -12,5 +12,6 @@ c = a + b;
 cout << "The sum of the entered numbers = " << c << endl;
 c = a - b;
 cout << "The difference between the entered numbers = " << c << endl;
-return 0;
+c = a / b;
+cout << "The division the entered numbers = " << c << endl;
 }
