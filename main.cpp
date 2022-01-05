ac38a7e7a461d838a602751af3de5f4734eea381
Dmitriy Pavlov <ooo_tek@mail.ru>
2022-01-05 20:32:28 +0500

    Добавили вычисление разности между числами

--- main.cpp
+++ main.cpp
@@ -10,4 +10,7 @@ cin >> a >> b;
 int c;
 c = a + b;
 cout << "The sum of the entered numbers = " << c << endl;
+c = a - b;
+cout << "The difference between the entered numbers = " << c << endl;
+return 0;
 }
